4274be3af28397b27dc273187722b4513d2477e2
thedudefreebird <tyler.freebery@gmail.com>
2019-11-07 11:42:36 -0500

    added psuedo to hlep complete functions

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ let QUESTIONS = [
   {
     question: 'How many teams are in the National Hockey League?',
     answers: ['30 teams', '38 teams', '35 teams', '31 teams'],
-    correctAnswer: '31 teams
+    correctAnswer: '31 teams'
   },
   {
     question: 'How many divisions are in the National Hockey League?',
@@ -48,29 +48,52 @@ function handleStartBtn(){
   $('.start-btn').on('click', function(){
       $('.start-btn').toggleClass('hide');
       $('.intro').toggleClass('hide');
-
+      handleSetQuestion();
   });
 
   console.log('Started!')
 }
 
 function handleSetQuestion(){
+  //create a var to hold html string (not global)
 
+  //header tag with QUESTIONS[questionCount].question
+    //add to html string
 
+  //for loop (iterate 4 times) //(let i = 0; i < 4; i++)
+    //add to html string, QUESTIONS[questionCount].answers
+
+  //submit button
+
+  //target DOM .append(html string from above)
+
+  handleAnswerSelection();
 }
 
 function handleAnswerSelection(){
+  //event listner for submit button being clicked
+
+  //event.preventDefault
+
+  //access user input and compare to answer in quesitons array
 
 
+  handleGrading();
 }
 
 function handleGrading(){
+  //if statment to verify if the correct answer was selected
+    //if true score++ quesitonCount++
+      //Correct! .append
 
-
+    //remove quesiton, set next question and include next button(event listner)
+    handleNextBtn();
 }
 
 function handleNextBtn(){
-
+  //event listener
+  //set new question.
+  //remove feedback from DOM(.append and empty/remove)
 
 }
 
